54ef8b0009b31e77a083040a25a9eb109709316e
Anthony Shew <anthonyshew@gmail.com>
2022-06-08 22:41:26 -0700

    Fix port.

--- .pulumi/index.ts
+++ .pulumi/index.ts
@@ -32,61 +32,60 @@ const main = async () => {
       region: digitalocean.Region.NYC3,
       // Build a Go project in the api/ directory that listens on port 3000
       // and serves it at https://foo.example.com/api
-      // services: [
-      //   {
-      //     alerts: [
-      //       {
-      //         operator: "GREATER_THAN",
-      //         rule: "CPU_UTILIZATION",
-      //         value: 75,
-      //         window: "TEN_MINUTES",
-      //       },
-      //     ],
-      //     environmentSlug: "go",
-      //     github: {
-      //       branch: "main",
-      //       deployOnPush: true,
-      //       repo: "anthonyshew/pulumi-do",
-      //     },
-      //     httpPort: 3000,
-      //     instanceCount: 2,
-      //     instanceSizeSlug: "professional-xs",
-      //     logDestinations: [
-      //       {
-      //         name: "MyLogs",
-      //         papertrail: {
-      //           endpoint: "syslog+tls://example.com:12345",
-      //         },
-      //       },
-      //     ],
-      //     name: "api",
-      //     routes: [
-      //       {
-      //         path: "/api",
-      //       },
-      //     ],
-      //     runCommand: "bin/api",
-      //     sourceDir: "api/",
-      //   },
-      // ],
-      // Builds a static site in the project's root directory
-      // and serves it at https://foo.example.com/
-      staticSites: [
+      services: [
         {
-          buildCommand: "npm run build",
+          alerts: [
+            {
+              operator: "GREATER_THAN",
+              rule: "CPU_UTILIZATION",
+              value: 75,
+              window: "FIVE_MINUTES",
+            },
+          ],
           github: {
             branch: "main",
             deployOnPush: true,
             repo: "anthonyshew/pulumi-do",
           },
-          name: "web",
+          httpPort: 8080,
+          instanceSizeSlug: "basic-xxs",
+          // logDestinations: [
+          //   {
+          //     name: "MyLogs",
+          //     papertrail: {
+          //       endpoint: "syslog+tls://example.com:12345",
+          //     },
+          //   },
+          // ],
+          name: "nextjs",
           routes: [
             {
               path: "/",
             },
           ],
+          buildCommand: "npm run build",
+          runCommand: "npm run start",
+          sourceDir: ".",
         },
       ],
+      // Builds a static site in the project's root directory
+      // and serves it at https://foo.example.com/
+      // staticSites: [
+      //   {
+      //     buildCommand: "npm run build",
+      //     github: {
+      //       branch: "main",
+      //       deployOnPush: true,
+      //       repo: "anthonyshew/pulumi-do",
+      //     },
+      //     name: "web",
+      //     routes: [
+      //       {
+      //         path: "/",
+      //       },
+      //     ],
+      //   },
+      // ],
     },
   });
 
