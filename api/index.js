0d0ff73097322ce9b1edeb2230d90baacd710e2b
Anthony Shew <anthonyshew@gmail.com>
2022-06-09 21:46:45 -0700

    Deployed stuff talks to each other?

--- api/index.js
+++ api/index.js
@@ -5,6 +5,6 @@ app.get("/test-route", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
 });
