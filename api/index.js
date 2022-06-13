0023d5c1b6d186ffb1aea5e6c2b8723ac9ded4ea
Anthony Shew <anthonyshew@gmail.com>
2022-06-13 14:58:36 -0700

    async in api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-const PrismaClient = require('@prisma/client').PrismaClient
+const PrismaClient = require("@prisma/client").PrismaClient;
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 var express = require("express");
 var app = express();
@@ -13,18 +13,17 @@ app.get("/test-route", (req, res, next) => {
   return res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.post('/write-new-user', (req, res) => {
+app.post("/write-new-user", async (req, res) => {
   const newUser = await prisma.user.create({
     data: {
       email: "test@test.com",
       name: "test",
-      password: "plaintextomg"
-    }
-  })
+      password: "plaintextomg",
+    },
+  });
 
-  return res.json(newUser)
-})
-
+  return res.json(newUser);
+});
 
 app.get("/secret", (req, res, next) => {
   return res.json({ secret: process.env.NEXT_PUBLIC_TEST_SECRET });
