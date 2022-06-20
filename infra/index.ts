e8458276fdfa414bd7a739029b25fc8d023dd69b
Anthony Shew <anthonyshew@gmail.com>
2022-06-19 23:03:55 -0700

    ECS task being weird...

--- infra/index.ts
+++ infra/index.ts
@@ -1,36 +1,42 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-import * as pulumi from '@pulumi/pulumi';
-import { TcpPorts, VpcSubnetArgs, SecurityGroup } from "@pulumi/awsx/ec2";
-import { InstanceType } from "@pulumi/aws/ec2";
-import { PrivateDnsNamespace, Service } from "@pulumi/aws/servicediscovery";
-import { VpcLink } from "@pulumi/aws/apigatewayv2";
-// Route table
-// Where a specific route should go =>
-//  Route from A => internetgateway (public subnet routing to internet)> Private subnet (private no external internet) => Route table in your private subnet add a NAT gateaway => allow to go to the internet General metadata
+
 const tags = {
+  // Question: Should we make this go off of the stack?
   ENVIRONMENT: process.env.ENV || "dev",
 };
-// Networking
-const setup = async () => {
-  // VPC => Virtual private cloud
-  // Public => All resources publicly available (webservers etc)
-  // Private => All resources that should not have a public interface
-  // but can connect to the internet with a NAT gateway. (Fargate)
-  // Internal => All resources that should NEVER have access to the internet (RDS)
-  const vpc = new awsx.ec2.Vpc("demo-vpc", {
+
+const main = async () => {
+  // Your project and stack info.
+  const project = pulumi.getProject();
+  const stack = pulumi.getStack();
+  const config = new pulumi.Config();
+
+  // Grab all of our config for this stack.
+  const region = config.require("region");
+  const dbPass = config.requireSecret("rds-password");
+  const dbPublic = config.requireBoolean("rds-public") ?? false;
+  const dbInstanceCount = config.requireNumber("rds-instance-count");
+  // Question: Couldn't get these to work.
+  // AWS automatically provisions to three availability zones by default.
+  // If we leave it out of the cluster declaration, it "just works".
+  // Why would we want to be explicit with this?
+  // const dbMultiAZ = ["a", "b", "c"].map((value) => region + value);
+
+  const vpc = new awsx.ec2.Vpc(`${project}-${stack}-vpc`, {
     cidrBlock: "10.0.0.0/16",
     subnets: [
       {
-        name: "demo-subnet-public",
+        name: `${project}-${stack}-subnet-public`,
         type: "public",
       },
       {
-        name: "demo-subnet-private",
+        name: `${project}-${stack}-subnet-private`,
         type: "private",
       },
       {
-        name: "demo-subnet-isolated",
+        name: ` ${project}-${stack}-subnet-isolated`,
         type: "isolated",
       },
     ],
@@ -38,84 +44,125 @@ const setup = async () => {
     tags,
   });
 
-  // Network security (Security Groups)
-  // HTTP/HTTPS network (public)
-  const sgWeb = new awsx.ec2.SecurityGroup("demo-sg-web", { vpc, tags });
-
-  awsx.ec2.SecurityGroupRule.ingress(
-    "http-access",
-    sgWeb,
-    new awsx.ec2.AnyIPv4Location(),
-    new TcpPorts(80)
-  );
-
-  awsx.ec2.SecurityGroupRule.ingress(
-    "https-access",
-    sgWeb,
-    new awsx.ec2.AnyIPv4Location(),
-    new TcpPorts(443)
-  );
-
-  awsx.ec2.SecurityGroupRule.egress(
-    "http-access",
-    sgWeb,
-    new awsx.ec2.AnyIPv4Location(),
-    new TcpPorts(80)
-  );
-
-  awsx.ec2.SecurityGroupRule.egress(
-    "https-access",
-    sgWeb,
-    new awsx.ec2.AnyIPv4Location(),
-    new TcpPorts(443)
-  );
-
-  const sgRDS = new awsx.ec2.SecurityGroup("demo-sg-rds", { vpc, tags });
-  awsx.ec2.SecurityGroupRule.ingress(
-    "rds-ingress",
-    sgRDS,
-    new awsx.ec2.AnyIPv4Location(),
-    new TcpPorts(5432)
-  );
-
-  awsx.ec2.SecurityGroupRule.egress(
-    "rds-egress",
-    sgRDS,
-    new awsx.ec2.AnyIPv4Location(),
-    new TcpPorts(5432)
-  );
-
-  // Application
-  // Subnet ids
-
+  // Question: These went unused, do we still want them?
   const publicSubnet = await vpc.getSubnets("public");
-  const privateSubnet = await vpc.getSubnets("private");
   const isolatedSubnet = await vpc.getSubnets("isolated");
 
-  // RDS instances
-  // const rdsInstance = new aws.rds.Instance("demo-instance", {
-  //   instanceClass: "db.t3.micro",
-  //   allocatedStorage: 20,
-  //   engine: "postgres",
-  //   username: "demo",
-  //   dbName: "test",
-  //   password: "demo123demo",
-  //   tags,
-  //   vpcSecurityGroupIds: [],
-  // });
+  const privateSubnets = await vpc.getSubnets("private");
 
-  // Fargate related
-  const ecsCluster = new awsx.ecs.Cluster("demo-ecs-cluster", {
+  // Network security (Security Groups)
+  // HTTP/HTTPS network (public)
+  const webSecurityGroup = new awsx.ec2.SecurityGroup(
+    `${project}-security-group-web`,
+    {
+      vpc,
+      tags,
+    }
+  );
+
+  const dbSecurityGroup = new awsx.ec2.SecurityGroup(
+    "networking-security-group-database",
+    {
+      vpc,
+      tags,
+    }
+  );
+
+  awsx.ec2.SecurityGroupRule.ingress(
+    "http-access",
+    webSecurityGroup,
+    new awsx.ec2.AnyIPv4Location(),
+    new awsx.ec2.TcpPorts(80)
+  );
+
+  awsx.ec2.SecurityGroupRule.ingress(
+    "https-access",
+    webSecurityGroup,
+    new awsx.ec2.AnyIPv4Location(),
+    new awsx.ec2.TcpPorts(443)
+  );
+
+  awsx.ec2.SecurityGroupRule.egress(
+    "http-access",
+    webSecurityGroup,
+    new awsx.ec2.AnyIPv4Location(),
+    new awsx.ec2.TcpPorts(80)
+  );
+
+  awsx.ec2.SecurityGroupRule.egress(
+    "https-access",
+    webSecurityGroup,
+    new awsx.ec2.AnyIPv4Location(),
+    new awsx.ec2.TcpPorts(443)
+  );
+
+  // Database
+  const dbSubnetGroup = new aws.rds.SubnetGroup(
+    `${project}-${stack}-db-subnet-group`,
+    {
+      name: `${project}-${stack}-db-subnet-group`,
+      subnetIds: vpc.getSubnetsIds("isolated"),
+      tags,
+    }
+  );
+
+  // Setup
+  const dbCluster = new aws.rds.Cluster(`${project}-${stack}-db-cluster`, {
+    clusterIdentifier: `${project}-${stack}-db`,
+    tags,
+    // Continued from question in the configs about if these are needed or not
+    // availabilityZones: ["a", "b", "c"].map((value) => region + value),
+    databaseName: `${project}-${stack}`,
+
+    // Database Engine Config
+    engine: "aurora-postgresql",
+    engineMode: "provisioned",
+    engineVersion: "14.3",
+    storageEncrypted: true,
+    serverlessv2ScalingConfiguration: {
+      maxCapacity: 16,
+      minCapacity: 1,
+    },
+
+    // Creds Config
+    masterUsername: "postgres",
+    masterPassword: dbPass,
+    iamDatabaseAuthenticationEnabled: true,
+    vpcSecurityGroupIds: [dbSecurityGroup.id],
+    dbSubnetGroupName: dbSubnetGroup.id,
+    skipFinalSnapshot: true,
+  });
+
+  // Scaling for Cluster Instances
+  let clusterInstances: aws.rds.ClusterInstance[] = [];
+
+  for (let i = 1; i <= dbInstanceCount; i++) {
+    clusterInstances.push(
+      new aws.rds.ClusterInstance(`${project}-${stack}-database-instance` + i, {
+        clusterIdentifier: dbCluster.id,
+        identifier: "instance" + "-" + i,
+
+        instanceClass: "db.serverless",
+        engine: dbCluster.engine.apply((value) => value as aws.rds.EngineType),
+        engineVersion: dbCluster.engineVersion,
+        publiclyAccessible: dbPublic,
+      })
+    );
+  }
+
+  const ecsCluster = new awsx.ecs.Cluster(`${project}-${stack}-ecs-cluster`, {
     vpc,
     tags,
   });
 
-  const namespace = new PrivateDnsNamespace("demo-namespace", {
+  const namespace = new aws.servicediscovery.PrivateDnsNamespace("namespace", {
     vpc: vpc.id,
     tags,
   });
 
-  const serviceRegistryHelloWorld = new Service("demo-hello-discover", {
+  // Service Register for Web
+  const serviceRegistryWeb = new aws.servicediscovery.Service("service-web", {
+    tags,
     dnsConfig: {
       namespaceId: namespace.id,
       dnsRecords: [
@@ -131,42 +178,85 @@ const setup = async () => {
     },
   });
 
-  const fargateServiceTask = new awsx.ecs.FargateTaskDefinition(
-    "demo-hello-world",
-    {
-      container: {
-        essential: true,
-        image: "pvermeyden/nodejs-hello-world:a1e8cf1edcc04e6d905078aed9861807f6da0da4",
-        portMappings: [
-          {
-            containerPort: 80,
-            hostPort: 80,
-          },
-        ],
-      },
-    }
-  );
+  const ecsWebTask = new awsx.ecs.FargateTaskDefinition("ecs-task-web", {
+    tags,
+    container: {
+      essential: true,
+      image:
+        "pvermeyden/nodejs-hello-world:a1e8cf1edcc04e6d905078aed9861807f6da0da4",
 
-  const fargateService = new awsx.ecs.FargateService("demo-fargate-service", {
+      logConfiguration: {
+        logDriver: "awslogs",
+        options: {
+          "awslogs-region": region,
+          "awslogs-group": "web-service",
+          "awslogs-create-group": "true",
+          "awslogs-stream-prefix": "test",
+        },
+      },
+      portMappings: [
+        {
+          containerPort: 80,
+          hostPort: 80,
+        },
+      ],
+    },
+  });
+
+  const ecsWebService = new awsx.ecs.FargateService("ecs-service", {
+    cluster: ecsCluster,
+
+    // Task Config and Scale
+    desiredCount: 1,
+    taskDefinition: ecsWebTask,
+
+    // Service Discovery
+    serviceRegistries: {
+      port: 80,
+      registryArn: serviceRegistryWeb.arn,
+    },
+
+    // Firewall and Networking
+    securityGroups: [webSecurityGroup],
+    subnets: privateSubnets.map((subnet) =>
+      subnet.subnet.id.apply((t) => t as string)
+    ),
+    assignPublicIp: false,
+
     deploymentCircuitBreaker: {
       enable: true,
       rollback: true,
     },
-    serviceRegistries: {
-      port: 80,
-      registryArn: serviceRegistryHelloWorld.arn,
-    },
-    assignPublicIp: false,
-    subnets: [privateSubnet[0].id],
-    cluster: ecsCluster,
-    desiredCount: 1,
-    taskDefinition: fargateServiceTask,
   });
 
-  // API Gateway stuff
-  const vpcLink = new VpcLink("vpcLink", {
-    subnetIds: [privateSubnet[0].id],
-    securityGroupIds: [],
+  const dbIngress = awsx.ec2.SecurityGroupRule.ingress(
+    "database-access-private",
+    dbSecurityGroup,
+    {
+      cidrBlocks: privateSubnets.map((subnet) =>
+        subnet.subnet.cidrBlock.apply((t) => t as string)
+      ),
+    },
+    new awsx.ec2.TcpPorts(5432)
+  );
+
+  const dbEgress = awsx.ec2.SecurityGroupRule.egress(
+    "database-access-private",
+    dbSecurityGroup,
+    {
+      cidrBlocks: privateSubnets.map((subnet) =>
+        subnet.subnet.cidrBlock.apply((t) => t as string)
+      ),
+    },
+    new awsx.ec2.TcpPorts(5432)
+  );
+
+  const vpcLink = new aws.apigatewayv2.VpcLink("api-vpcLink", {
+    tags,
+    subnetIds: privateSubnets.map((subnet) =>
+      subnet.id.apply((t) => t as string)
+    ),
+    securityGroupIds: [webSecurityGroup.id],
   });
 
   const apiGateway = new aws.apigatewayv2.Api("api", {
@@ -177,36 +267,23 @@ const setup = async () => {
   const cloudMapIntegration = new aws.apigatewayv2.Integration("integration", {
     integrationType: "HTTP_PROXY",
     integrationMethod: "ANY",
-    integrationUri: serviceRegistryHelloWorld.arn,
+    integrationUri: serviceRegistryWeb.arn,
     connectionType: "VPC_LINK",
     connectionId: vpcLink.id,
     apiId: apiGateway.id,
   });
 
-const helloWorldRoute = new aws.apigatewayv2.Route("helloWorldRoute", {
+  const webProxyRoute = new aws.apigatewayv2.Route("api-route", {
     apiId: apiGateway.id,
     routeKey: "ANY /{proxy+}",
     target: pulumi.interpolate`integrations/${cloudMapIntegration.id}`,
-});
-const stageHelloWorld= new aws.apigatewayv2.Stage("example", {apiId: apiGateway.id, autoDeploy: true});
+  });
 
-  //Cloud map
-  // Service discovery with Route53
-  // Discover services based on DNS.
-  // Goal: Find service, in private subnet with x parameters
-  // Service local hosted zone => entry ab.local => ab -> service
-  // Creation FargateService => enable service discovery <-> cloud map
-  // All instances of a specific service are discovered and can be routed to
-
-  // kittr.gg/api/blabla => API GATEWAY ROUTE => /api/blabla
-  // API Gateway <=> API Proxy routes several routes to specific targets(lambda, http url, websockets, ...)
-  // API Gateway in public subnet (VPCLink) (api gateway (on specific routes to your private subnet))
-  // HTTP, REST (applicable for this use case) => HTTP
-  // Create a path "/" => ANY: {proxy+} => several options one of them
-  // => ALB/NLB / Cloud map -> created service
-  // Add a NAT Gateway on your VPC => route all internal trafic to your NAT GATEWAY
-
-  // RDS => residing in your private subnet (=> VPC)
-  // RDS to communicate with your Fargate service => Service x should be able to connect to 5432 (PG)
+  const webStageGateway = new aws.apigatewayv2.Stage("api-gateway-stage", {
+    tags,
+    apiId: apiGateway.id,
+    autoDeploy: true,
+  });
 };
-setup();
+
+main();
